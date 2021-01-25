58433e3d1fba823f98e902f15e1e06902666d680
Bjørnar Hagen <bjornar@datahjelpen.no>
2021-01-25 02:05:13 +0100

    Add existing website

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>XDH</title>
+	<link rel="stylesheet" href="/style.css">
+</head>
+<body>
+	<section class="layout-full-center">
+		<img src="/assets/images/brand/xdh-logo-wb.png" alt="XDH logo">
+		<p>When exquisite quality is required, we are the correct choice.</p>
+		<a href="#" class="button">Let's talk -></a>
+	</section>
+</body>
+</html>
